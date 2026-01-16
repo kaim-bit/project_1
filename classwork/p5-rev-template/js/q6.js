57a0182efb379aefd871fdc684b894a1ca2fa193
crooksnwhiskey <alex.chardon36@gmail.com>
2026-01-15 21:10:27 -0500

    q6

--- classwork/p5-rev-template/js/q6.js
+++ classwork/p5-rev-template/js/q6.js
@@ -1,11 +1,35 @@
 "use strict";
 
+
 function setup() {
     console.log("go")
-
+    createCanvas(600, 600)
 }
 
 function draw() {
+    background("#87ceeb")
+    push();
+    textSize(28);
+    fill("white");
+    noStroke();
+    text('test', width / 2, width / 2);
+    pop();
 
+    for (let i = 0; i < 10; i++) {
+        push();
+        textSize(28);
+        fill("white");
+        noStroke();
+        text(str(i), i * 4 * 10, 25);
+        pop();
+    }
 
+    for (let i = 0; i < 16; i++) {
+        push();
+        textSize(28);
+        fill("white");
+        noStroke();
+        text(str(i), 10, i * 4 * 10);
+        pop();
+    }
 }
