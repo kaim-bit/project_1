734522198efcf9900d5b18011a437c5654eb6908
crooksnwhiskey <alex.chardon36@gmail.com>
2026-02-11 14:08:44 -0500

    starting to make title page
    just put a button that does nothing so far

---
+++ pop-up-party/js/popup.js
@@ -0,0 +1,12 @@
+
+
+const startButton = document.getElementById("play-button");
+const titleScreen = document.getElementById("title");
+
+startButton.addEventListener("click", () => {
+    startGame();
+});
+
+function startGame() {
+    console.log("hello");
+}
